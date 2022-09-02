36fd859867220c68479d2c12ee382db1243f5da3
Robin Mueller <robin.mueller.m@gmail.com>
2022-09-02 09:07:20 +0200

    this seems to work

---
+++ start/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "fsfw/serviceinterface.h"
+#include "FSFWConfig.h"
+
+using namespace std;
+
+
+#if FSFW_CPP_OSTREAM_ENABLED == 1
+ServiceInterfaceStream sif::debug("DEBUG", false);
+ServiceInterfaceStream sif::info("INFO", false);
+ServiceInterfaceStream sif::warning("WARNING", false);
+ServiceInterfaceStream sif::error("ERROR", false, true, true);
+#endif
+
+
+int main() {
+    cout << "hello world!" << endl;
+}
