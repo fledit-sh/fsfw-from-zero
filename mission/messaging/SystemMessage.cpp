eef4e5db04e37558eefd7db35926a75f7a633de3
noel <noel@ernsting-luz.de>
2025-10-01 17:56:31 +0200

    VerificationReporter was constructing CommandingService with defalult nullprt

--- mission/messaging/SystemMessage.cpp
+++ mission/messaging/SystemMessage.cpp
@@ -1,3 +1,18 @@
+/**************************************************************
+*  Project      : FSFWWebcamDemo
+ *  Modul        : SW Development for Spacecraft
+ *
+ *  Autor        : Noel Ernsting Luz
+ *  Co-Autor     : GPT-5 (KI-unterstützt)
+ *  Erstellt am  : 2025-10-01
+ *  Version      : 1.0
+ *
+ *  Hinweise     :
+ *   - Teile des Codes wurden von GPT-5 generiert und
+ *     von einem Menschen überprüft, angepasst und erweitert.
+ *
+ **************************************************************/
+
 #include "mission/messaging/SystemMessage.h"
 
 #include "fsfw/ipc/CommandMessage.h"
--- mission/webcam/WebcamCookie.h
+++ mission/webcam/WebcamCookie.h
@@ -1,6 +1,18 @@
-//
-// Created by noel on 01.10.25.
-//
+/**************************************************************
+*  Project      : FSFWWebcamDemo
+ *  Modul        : SW Development for Spacecraft
+ *
+ *  Autor        : Noel Ernsting Luz
+ *  Co-Autor     : GPT-5 (KI-unterstützt)
+ *  Erstellt am  : 2025-10-01
+ *  Version      : 1.0
+ *
+ *  Hinweise     :
+ *   - Teile des Codes wurden von GPT-5 generiert und
+ *     von einem Menschen überprüft, angepasst und erweitert.
+ *
+ **************************************************************/
+
 #pragma once
 
 #include <fsfw/devicehandlers/CookieIF.h>
