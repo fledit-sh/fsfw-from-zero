4cb4da38fd7bdaea2b3ca5737e36a5d1852d6a05
noel <noel@ernsting-luz.de>
2025-10-01 13:08:24 +0200

    Here comes the art of messing around. Im both good and bad at it and this is definitely sth that does not work as a main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
 // Necessary header files for input output functions
 #include <iostream>
+#include "fsfw/objectmanager.h"
+#include "fsfw/tasks/TaskFactory.h"
+
+#include "mission/WebcamDeviceHandler.h"
+#include "mission/webcam/WebcamCookie.h"
+#include "mission/webcam/WebcamDefinitions.h"
+
+#include <chrono>
+#include <memory>
+#include <thread>
 using namespace std;
 
 // main() function: where the execution of
 // C++ program begins
 int main() {
+    auto* objectManager = ObjectManager::instance();
+    auto webcamCookie = std::make_unique<WebcamCookie>("/dev/video0", 30.0);
+    auto* webcamHandler = new WebcamDeviceHandler(
+        webcam::objectIdWebcamHandler, 0, webcamCookie.get(), nullptr, 20);
+    (void)webcamHandler;
 
-    // This statement prints "Hello World"
-    cout << "Hello World";
+    objectManager->initialize();
 
+    auto* taskFactory = TaskFactory::instance();
+    auto priority = 0;
+    PeriodicTaskIF* webcamTask = taskFactory->createPeriodicTask(
+        "WEBCAM_TASK", priority, PeriodicTaskIF::MINIMUM_STACK_SIZE, 1.0, nullptr);
+    webcamTask->addComponent(webcam::objectIdWebcamHandler);
+    webcamTask->startTask();
+    using namespace std::chrono_literals;
+    while (true) {
+        std::this_thread::sleep_for(5s);
+    }
     return 0;
 }
